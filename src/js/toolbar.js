20c4ab6055dafc2343666cb1f842cf538ecd78cf
Bugaevsky <nbugaev@list.ru>
2020-07-05 00:59:45 +0400

    Task 2: first version

---
+++ src/js/toolbar.js
@@ -0,0 +1,23 @@
+function chooseMode(mode, elmnt) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.color = "";
+    }
+
+    // Show the specific tab content
+    document.getElementById(mode).style.display = "block";
+
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.color = '#23aea3';
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
