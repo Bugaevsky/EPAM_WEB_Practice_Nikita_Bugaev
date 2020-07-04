7fd7c259d8da0cc072c7ef67337b74c4d9ad90c1
Bugaevsky <nbugaev@list.ru>
2020-07-05 01:50:49 +0400

    magic numbers

--- src/js/toolbar.js
+++ src/js/toolbar.js
@@ -1,21 +1,21 @@
 function chooseMode(mode, elmnt) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+    // Hide all elements with class="activeText" by default */
+    var i, activeText, barButton;
+    activeText = document.getElementsByClassName("activeText");
+    for (i = 0; i < activeText.length; i++) {
+        activeText[i].style.display = "none";
     }
 
-    // Remove the color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.color = "";
+    // Remove the color of all barButton/buttons
+    barButton = document.getElementsByClassName("barButton");
+    for (i = 0; i < barButton.length; i++) {
+        barButton[i].style.color = "";
     }
 
-    // Show the specific tab content
+    // Show the specific barButton
     document.getElementById(mode).style.display = "block";
 
-    // Add the specific color to the button used to open the tab content
+    // Add the specific color to the button used to open barButton
     elmnt.style.color = '#23aea3';
 }
 
